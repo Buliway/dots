2c4b58c9d7916082a8cbaed45f2aad017fd46e62
Buliway <me@buliway.ru>
2024-03-13 02:20:45 +0300

    Изменил конфиг fish shell

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,3 +8,17 @@ if test -z (pgrep ssh-agent | string collect)
     set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -Ux SSH_AGENT_PID $SSH_AGENT_PID
 end
+
+# Переменные для n, который управляет версиями node. Аналог nvm
+set -gx N_PREFIX $HOME/.n
+set -gx fish_user_paths $N_PREFIX/bin $fish_user_paths
+
+# Команда ya работает как yazi, но при выходе переносит меня в каталог, где я был в yazi
+function ya
+	set tmp (mktemp -t "yazi-cwd.XXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
